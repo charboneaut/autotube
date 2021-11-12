fd11fec004d0e795ac2adc5862694916a6471736
charboneaut <charboneau@protonmail.ch>
2021-11-12 12:18:39 -0500

    moved blue collar code to helpers.py

---
+++ helpers.py
@@ -0,0 +1,106 @@
+import argparse
+import re
+import requests
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC
+from moviepy.editor import VideoFileClip
+
+
+def command_line_parser():
+    # command arguments
+    parser = argparse.ArgumentParser(description="Input the link that you want to download. Has some flags thay modify behavior.")
+    parser.add_argument("-s", action="store_true", dest="silence", help="silences the prompts for title and artist")
+    parser.add_argument("link", type=str, help="yt link that you want to download")
+    return parser.parse_args()
+
+
+def parse_title(title):
+    """looks through the title of the video for the author & song name.
+    will try to find remix name and append that to the title as well"""
+    artist = ""
+    track = ""
+
+    if title[0] == "[":
+        while title[0] != "-":
+            title = title[1:]
+        title = title[1:]
+        title = title.strip()
+
+    for char in title:
+        if char in "-":
+            break
+        artist = artist + char
+
+    broken = False
+
+    for char in title:
+        if char in "-":
+            broken = True
+            continue
+        if not broken:
+            continue
+        else:
+            if char in "[(":
+                break
+            else:
+                track = track + char
+
+    artist = artist.strip()
+    track = track.strip()
+
+    if "remix" in title or "Remix" in title:
+        rem = re.findall("[a-z]* remix", title, re.IGNORECASE)[0]
+        track = f"{track} - {rem}"
+        track = track.strip()
+
+    return {"artist": artist, "track": track}
+
+
+def song_questionare(track_info, ns):
+    """uses info from parse_track to question the user about their song.
+    replaces info from parse_track upon user request"""
+    track = track_info["track"]
+    artist = track_info["artist"]
+
+    if not ns.silence:
+        track_yea_or_nay = input(f"Is the song's title \"{track}\"? (y/n)")
+
+    if track_yea_or_nay.lower() != "y":
+        track = input("What is the song's title?")
+
+    artist_yea_or_nay = input(f"Is the song's artist \"{artist}\"? (y/n)")
+
+    if artist_yea_or_nay.lower() != "y":
+        artist = input("What is the song's artist?")
+
+    return {"artist": artist, "track": track}
+
+
+def mp4_to_mp3(track):
+    """converts .mp4 videos to .mp3 files.
+    doesn't work with .mp4 audio only files"""
+    video = VideoFileClip(f"{track}.mp4")
+    audio = video.audio
+    audio.write_audiofile(f"{track}.mp3")
+    video.close()
+    audio.close()
+
+
+def apply_apic(track, thumbnail_url):
+    """gets the video's thumbnail from yt and applies it to
+    the audio file as an APIC id3 tag. img_data has to be byte string"""
+    img_data = requests.get(thumbnail_url).content
+    audio = ID3(f"{track}.mp3")
+    audio.add(APIC(mime="jpg", data=img_data, type=3))
+    audio.save(f"{track}.mp3")
+
+
+def apply_id3(track_info):
+    """adds artist and title to id3 tags"""
+    track = track_info["track"]
+    artist = track_info["artist"]
+
+    audio_e = EasyID3(f"{track}.mp3")
+    audio_e["artist"] = artist
+    audio_e["title"] = track
+    audio_e.save()
--- main.py
+++ main.py
@@ -1,115 +1,30 @@
 import os
-import argparse
-import re
 from pytube.__main__ import YouTube
-import requests
-from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3, APIC
-from moviepy.editor import VideoFileClip
+from helpers import command_line_parser, parse_title, song_questionare, mp4_to_mp3, apply_apic, apply_id3
 
 
 def main():
-
-    parser = argparse.ArgumentParser(description="Input the link that you want to download. Has some flags thay modify behavior.")
-
-    parser.add_argument("-s", action="store_true", dest="silence", help="silences the prompts for title and artist")
-    parser.add_argument("link", type=str, help="yt link that you want to download")
-    
-
-    ns = parser.parse_args()
-
-    # Getting the .mp4 file from yt
+    ns = command_line_parser()
 
     yt = YouTube(ns.link)
-
     stream = yt.streams.filter(
         only_audio=False, audio_codec="mp4a.40.2").order_by("abr").desc()
 
-    title = stream[0].title
+    track_info = parse_title(stream[0].title)
+    track = track_info["track"]
+    artist = track_info["artist"]
 
-    artist = ""
-
-    track = ""
-
-    if title[0] == "[":
-        while title[0] != "-":
-            title = title[1:]
-        title = title[1:]
-        title = title.strip()
-
-    for char in title:
-        if char in "-":
-            break
-        artist = artist + char
-
-    broken = False
-
-    for char in title:
-        if char in "-":
-            broken = True
-            continue
-        if not broken:
-            continue
-        else:
-            if char in "[(":
-                break
-            else:
-                track = track + char
-
-    artist = artist.strip()
-    track = track.strip()
-
-    if "remix" in title or "Remix" in title:
-        rem = re.findall("[a-z]* remix", title, re.IGNORECASE)[0]
-        track = f"{track} - {rem}"
-        track = track.strip()
-
-    if not ns.silence:
-        track_yea_or_nay = input(f"Is the song's title \"{track}\"? (y/n)")
-
-        if track_yea_or_nay.lower() != "y":
-            track = input("What is the song's title?")
-
-        artist_yea_or_nay = input(f"Is the song's artist \"{artist}\"? (y/n)")
-
-        if artist_yea_or_nay.lower() != "y":
-            artist = input("What is the song's artist?")
+    track_info = song_questionare(track_info, ns)
+    track = track_info["track"]
+    artist = track_info["artist"]
 
     print("Downloading...")
-
     stream[0].download(filename=f"{track}.mp4")
 
-    # Converting file from .mp4 to .mp3
+    mp4_to_mp3(track)
 
-    video = VideoFileClip(f"{track}.mp4")
-
-    audio = video.audio
-
-    audio.write_audiofile(f"{track}.mp3")
-
-    video.close()
-
-    audio.close()
-
-    # Applying APIC ID3 tag
-
-    img_data = requests.get(yt.thumbnail_url).content
-
-    audio = ID3(f"{track}.mp3")
-
-    audio.add(APIC(mime="jpg", data=img_data, type=3))
-
-    audio.save(f"{track}.mp3")
-
-    # Applying easy tags
-
-    audio_e = EasyID3(f"{track}.mp3")
-
-    audio_e["artist"] = artist
-
-    audio_e["title"] = track
-
-    audio_e.save()
+    apply_apic(track, yt.thumbnail_url)
+    apply_id3(track_info)
 
     os.remove(f"{track}.mp4")
 
